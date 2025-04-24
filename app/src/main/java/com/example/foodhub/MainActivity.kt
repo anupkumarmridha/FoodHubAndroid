41b659c429fab50214dfd1a588248e480c82d13a
anupkumarmridha <anupkumarmridha.net@gmail.com>
2025-04-24 17:07:48 +0530

    Enhance navigation transitions in MainActivity; add enter and exit animations for composable screens

--- app/src/main/java/com/example/foodhub/MainActivity.kt
+++ app/src/main/java/com/example/foodhub/MainActivity.kt
@@ -8,6 +8,10 @@ import android.view.animation.OvershootInterpolator
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.animation.AnimatedContentTransitionScope
+import androidx.compose.animation.core.tween
+import androidx.compose.animation.fadeIn
+import androidx.compose.animation.fadeOut
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
@@ -94,7 +98,31 @@ class MainActivity : ComponentActivity() {
                             navController=navController,
                             startDestination = AuthScreen,
                             modifier = Modifier
-                                .padding(innerPadding)
+                                .padding(innerPadding),
+                            enterTransition = {
+                                slideIntoContainer(
+                                    towards = AnimatedContentTransitionScope.SlideDirection.Left,
+                                    animationSpec = tween(400)
+                                )+fadeIn(animationSpec = tween(400))
+                            },
+                            exitTransition = {
+                                slideOutOfContainer(
+                                    towards = AnimatedContentTransitionScope.SlideDirection.Right,
+                                    animationSpec = tween(400)
+                                )+ fadeOut(animationSpec = tween(400))
+                            },
+                            popEnterTransition = {
+                                slideIntoContainer(
+                                    towards = AnimatedContentTransitionScope.SlideDirection.Right,
+                                    animationSpec = tween(400)
+                                )+fadeIn(animationSpec = tween(400))
+                            },
+                            popExitTransition = {
+                                slideOutOfContainer(
+                                    towards = AnimatedContentTransitionScope.SlideDirection.Left,
+                                    animationSpec = tween(400)
+                                )+ fadeOut(animationSpec = tween(400))
+                            },
                         ){
                             composable<SignUp>{
                                 SignUpScreen(navController)
@@ -102,7 +130,34 @@ class MainActivity : ComponentActivity() {
                             composable<AuthScreen>{
                                 AuthScreen(navController)
                             }
-                            composable<Login>{
+                            composable<Login>(
+
+//                                enterTransition = {
+//                                    slideIntoContainer(
+//                                        towards = AnimatedContentTransitionScope.SlideDirection.Up,
+//                                        animationSpec = tween(300)
+//                                    )+fadeIn(animationSpec = tween(300))
+//                                },
+//                                exitTransition = {
+//                                    slideOutOfContainer(
+//                                        towards = AnimatedContentTransitionScope.SlideDirection.Up,
+//                                        animationSpec = tween(300)
+//                                    )+ fadeOut(animationSpec = tween(300))
+//                                },
+//                                popEnterTransition = {
+//                                    slideIntoContainer(
+//                                        towards = AnimatedContentTransitionScope.SlideDirection.Down,
+//                                        animationSpec = tween(300)
+//                                    )+fadeIn(animationSpec = tween(300))
+//                                },
+//                                popExitTransition = {
+//                                    slideOutOfContainer(
+//                                        towards = AnimatedContentTransitionScope.SlideDirection.Down,
+//                                        animationSpec = tween(300)
+//                                    )+ fadeOut(animationSpec = tween(300))
+//                                },
+//
+                            ){
                                 LoginScreen(navController)
                             }
                             composable<Home>{
